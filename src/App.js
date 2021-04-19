bb73459d70b7f2d94c64f22dc402399cb402d63d
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2021-04-19 23:45:08 +1200

    Edited mui-kit template

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import SendResetPasswordLinkPage from "./pages/SendResetPasswordLinkPage";
-import ConfirmationPage from "./pages/ConfirmationPage";
-import ResetPasswordPage from "./pages/ResetPasswordPage";
-import LoginPage from "./pages/LoginPage";
-import DemoHomePage from "./pages/DemoHomePage";
-import DashboardMingyang from "./pages/DashboardMingyang";
-import DashboardHarpreet from './pages/DashboardHarpreet';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import { useRoutes } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+import GlobalStyles from 'src/components/GlobalStyles';
+import 'src/mixins/chartjs';
+import theme from 'src/theme';
+import routes from 'src/routes';
 
 const App = () => {
-  return (
-    <div className="App">
-      <h2>App.js</h2>
-      <Router>
-        <Route exact path="/" component={DemoHomePage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/resetpassword" component={ResetPasswordPage} />
-        <Route exact path="/sendresetlink" component={SendResetPasswordLinkPage}/>
-        <Route exact path="/confirm" component={ConfirmationPage} />
+  const routing = useRoutes(routes);
 
-        {/* Need to implement protected routes for pages below as
-        they can only be visible once authorised and authenticated */}
-        <Route exact path="/mingyang" component={DashboardMingyang} />
-        <Route exact path="/harpreet" component={DashboardHarpreet} />
-      </Router>
-    </div>
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {routing}
+    </ThemeProvider>
   );
 };
+
 export default App;
